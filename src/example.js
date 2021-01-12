75d9ad31c3bbb1fa259ed90306e52a5376479dff
naoya7076 <naoya7076@gmail.com>
2021-01-12 22:04:39 +0900

    eslintを追加してそれに伴う修正

--- src/example.js
+++ src/example.js
@@ -1,12 +1,9 @@
-'use strict'
-
 const username = 'freddy'
 typeof username === 'string'
 
-if(!'serviceWorker' in navigator){
-
+if(!('serviceWorker' in navigator)){
 }
 
 const greeting = 'hello'
-console.log(`${greeting} world!`)
+console.log(`${greeting} world!`);
 [1,2,3].forEach(x => console.log(x))
